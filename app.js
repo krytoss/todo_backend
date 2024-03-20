9eecc1660d5421a8d086a880cf627aa8d70eba36
BuildTools <kopcsanyiludovit@gmail.com>
2024-03-20 10:15:07 +0100

    some formatting changes

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const app = express()
-const dotenv = require('dotenv').config()
+const dotenv = require('dotenv')
+const bodyParser = require('body-parser')
 
+dotenv.config()
+
+// Configuring the app to use a bodyParser for getting data from requests
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+
+// ROUTES
+// ====================================================
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
+
+// STARTING THE SERVER
+// =====================================================
 app.listen(process.env.PORT, () => {
     console.log(`Listening on port ${process.env.PORT}`)
 })
