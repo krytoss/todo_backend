1400f01fa75af24987f47da1df19c865bf1e25ac
BuildTools <kopcsanyiludovit@gmail.com>
2024-03-20 10:22:38 +0100

    added postgresql and script for running through nodemon

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
+const db = require('./db')
 
 dotenv.config()
 
---
+++ db.js
@@ -0,0 +1,16 @@
+const { Pool } = require('pg')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const pool = new Pool({
+    user:       process.env.DB_USER,
+    password:   process.env.DB_PASS,
+    host:       process.env.DB_HOST,
+    port:       process.env.DB_PORT,
+    database:   process.env.DB_NAME
+})
+
+module.export = {
+    query: (text, params) => pool.query(text, params)
+}
